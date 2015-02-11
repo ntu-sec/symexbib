a9d7100844b34a12f1b3a5c29862daba3e705368
HongxuChen <leftcopy.chx@gmail.com>
2015-02-11 19:42:52 +0800

    all SE

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # remove section read/rating/date-added/date-modified/abstract/local-url/file
-# bibtex -> markdown (accents)
+# remove accents
 
 from __future__ import print_function
 import os
@@ -20,45 +20,48 @@ def delKey(entry, key):
     if key in entry:
         del entry[key]
 
+def Englishize(s):
+    return re.sub(r'(\\[\w\"\'\`\^]|\{|\})', '', s)
+
 if len(sys.argv) < 2:
     print("usage: {} bibtex_file".format(sys.argv[0]))
     exit(1)
 
 filename = sys.argv[1]
 
-with open(filename) as bibtex_file:
+with open(filename, 'r') as bibtex_file:
     bib_database = bibtexparser.load(bibtex_file)
 
-PREAMBLE = '''|Title|Venue|Year|Authors|
+PREAMBLE = '''|Title|Year|Venue|Authors|
 |-----|-----|----|-------|
 '''
 mkd_fname = os.path.splitext(filename)[0] + '.md'
 outlist = []
-pat_year = r"'?[0-9]{2,4}.*$"
-sort_key = lambda entry: str(getValue(entry, 'year')) + '::' + getValue(entry, 'id') 
-entry_list = sorted(bib_database.get_entry_list(), key=sort_key, reverse=True)
+entry_list = bib_database.get_entry_list()
 for entry in entry_list:
-    raw_title = getValue(entry, 'title')
-    link =getValue(entry, 'link')
-    title = '[' + raw_title + '](' + link + ')'
-###
+    year = getValue(entry, 'year')
     venue = ''
     if 'booktitle' in entry:
         venue = entry['booktitle']
     if 'journal' in entry:
         venue = entry['journal']
-    venue = re.sub(pat_year, '', venue)
-###
-    year = getValue(entry, 'year')
+    title = getValue(entry, 'title')
+    link = getValue(entry, 'link')
     author = getValue(entry, 'author')
-    out = '|' + '|'.join([title, venue, year, author]) + '|'
-    outlist.append(out)
-entries = '\n'.join(outlist)
+    author = Englishize(author)
+    if '\\' in author:
+        print(repr(author))
+    outlist.append((year, venue, title, link, author))
+outlist.sort(key=lambda tup: str(tup[0]) + tup[1], reverse=True)
+
 with open(mkd_fname, 'w') as md:
     md.write(PREAMBLE)
-    md.write(entries)
+    for tup in outlist:
+        title = '[' + tup[2] + '](' + tup[3] + ')'
+        out = '|' + ' |'.join([title, tup[0], tup[1], tup[4]]) + '|\n'
+        md.write(out)
 
-for entry in bib_database.entries:
+for entry in entry_list:
     for k in ['read', 'rating', 'date-added', 'date-modified', 'language', 'uri', 'abstract', 'local-url', 'file']:
         delKey(entry, k)
 with open(filename, 'w') as newbib:
